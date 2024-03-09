66c888da786e318711df0f9a586befcd95f05e10
Berenguer Pou <berenguer.pou@gmail.com>
2024-03-10 00:17:00 +0100

    fix test

--- back-end-app/src/app.controller.spec.ts
+++ back-end-app/src/app.controller.spec.ts
@@ -16,7 +16,9 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe(
+        'Hello World from Codelab CICD TRP / Berenguer Nest.js is working!',
+      );
     });
   });
 });

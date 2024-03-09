e93cd1274ef12a3fbcae0b1d45a0af28d6414fb7
Berenguer Pou <berenguer.pou@gmail.com>
2024-03-09 23:34:41 +0100

    add netlify deploy step in frontend workflow

--- front-end-app/src/App.tsx
+++ front-end-app/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -16,7 +16,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Codelab CI/CD - Curso Ingeniería del Software</h1>
+      <h2>The Refactor Project</h2>
+      <h3>Frontend App</h3>
+      <p>Developer: Berenguer Pou</p>
+      <p>Entrega: 9/03/2024</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
